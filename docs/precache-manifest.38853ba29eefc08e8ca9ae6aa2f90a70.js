818159d49b911beda98af713ece8ce3676a74407
Luis G <luis.g.publicidad@gmail.com>
2020-10-21 23:36:27 -0600

    docs added

---
+++ docs/precache-manifest.38853ba29eefc08e8ca9ae6aa2f90a70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88246e0dfa1a05811fcddd138c8fa370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea918a5945c0c60ae52",
+    "url": "/static/css/main.cd9f0a46.chunk.css"
+  },
+  {
+    "revision": "b649bd7ad8502b1040ca",
+    "url": "/static/js/2.0c71b2e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c71b2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea918a5945c0c60ae52",
+    "url": "/static/js/main.a8338a1e.chunk.js"
+  },
+  {
+    "revision": "1ae40933558171e31939be9509939ade",
+    "url": "/static/js/main.a8338a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623c3c45e3c1446f66ef",
+    "url": "/static/js/runtime-main.6b221cc4.js"
+  }
+]);
