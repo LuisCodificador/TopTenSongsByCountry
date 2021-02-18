e07ae647114bacb40d4272d1d88dafce8ad41e44
Luis G <luis.g.publicidad@gmail.com>
2021-02-17 22:48:30 -0800

    New build generated and renamed as docs. Previous docs renamed as DocsPrevio

---
+++ docs/precache-manifest.7ddd0bd1a1fd339b617ee134d33a2b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501687b67a9a301b68a1482799c274ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fabf7373a78311a95088",
+    "url": "/static/css/main.99f12eb0.chunk.css"
+  },
+  {
+    "revision": "e30160282419af6664b7",
+    "url": "/static/js/2.9bf3dedd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9bf3dedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabf7373a78311a95088",
+    "url": "/static/js/main.fabf035a.chunk.js"
+  },
+  {
+    "revision": "1ae40933558171e31939be9509939ade",
+    "url": "/static/js/main.fabf035a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623c3c45e3c1446f66ef",
+    "url": "/static/js/runtime-main.6b221cc4.js"
+  }
+]);
--- docsPrevio/service-worker.js
+++ docsPrevio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ddd0bd1a1fd339b617ee134d33a2b38.js"
+  "/precache-manifest.38853ba29eefc08e8ca9ae6aa2f90a70.js"
 );
 
 self.addEventListener('message', (event) => {
